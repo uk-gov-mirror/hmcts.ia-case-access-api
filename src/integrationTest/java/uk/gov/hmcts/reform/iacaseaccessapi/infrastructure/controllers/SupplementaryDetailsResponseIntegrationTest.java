cdad56808910b06d3e0d019b2d376b05d7cbc409
Brad Sorour <brad.sorour@gmail.com>
2021-06-16 09:29:06 +0100

    missing surname integration test

--- src/integrationTest/java/uk/gov/hmcts/reform/iacaseaccessapi/infrastructure/controllers/SupplementaryDetailsResponseIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/iacaseaccessapi/infrastructure/controllers/SupplementaryDetailsResponseIntegrationTest.java
@@ -15,13 +15,16 @@ import uk.gov.hmcts.reform.iacaseaccessapi.testutils.*;
 
 public class SupplementaryDetailsResponseIntegrationTest extends SpringBootIntegrationTest  implements WithServiceAuthStub, WithCoreCaseApiStub, WithIdamStub {
 
-    @org.springframework.beans.factory.annotation.Value("classpath:ccd-search-result-respons.json")
+    @org.springframework.beans.factory.annotation.Value("classpath:ccd-search-result-response.json")
     private Resource resourceFile;
 
-    @org.springframework.beans.factory.annotation.Value("classpath:ccd-search-result-empty-respons.json")
+    @org.springframework.beans.factory.annotation.Value("classpath:ccd-search-result-missing-surname-response.json")
+    private Resource missingSurnameResourceFile;
+
+    @org.springframework.beans.factory.annotation.Value("classpath:ccd-search-result-empty-response.json")
     private Resource emptyResponseResourceFile;
 
-    @org.springframework.beans.factory.annotation.Value("classpath:ccd-search-result-error-respons.json")
+    @org.springframework.beans.factory.annotation.Value("classpath:ccd-search-result-error-response.json")
     private Resource errorResponseResourceFile;
 
     private final String fullResponseRequest =  "{\"ccd_case_numbers\":["
@@ -37,6 +40,9 @@ public class SupplementaryDetailsResponseIntegrationTest extends SpringBootInteg
                                                 + "\"22222222222222\","
                                                 + "\"99999999999999\"]}";
 
+    private final String missingSurnameResponseRequest =  "{\"ccd_case_numbers\":["
+                                                          + "\"1619513189387099\"]}";
+
     private final String fullResponse = "{\"supplementary_info\":["
                                   + "{\"ccd_case_number\":\"1619513189387098\","
                                   + "\"supplementary_details\":{\"surname\":\"Johnson\"}}]}";
@@ -50,6 +56,10 @@ public class SupplementaryDetailsResponseIntegrationTest extends SpringBootInteg
     private final String emptyResponse = "{\"supplementary_info\":[],"
                                       + "\"missing_supplementary_info\":"
                                     + "{\"ccd_case_numbers\":[\"1619513189387090\",\"22222222222222\",\"99999999999999\"]}}";
+
+    private final String missingSurnameResponse = "{\"supplementary_info\":["
+                                                  + "{\"ccd_case_number\":\"1619513189387099\","
+                                                  + "\"supplementary_details\":{\"surname\":\"\"}}]}";
 
     void setup(WireMockServer server) {
         addUserInfoStub(server);
@@ -137,4 +147,26 @@ public class SupplementaryDetailsResponseIntegrationTest extends SpringBootInteg
             .andReturn();
 
     }
+
+    @Test
+    public void should_return_200_status_code_for_missing_surname(
+        @WiremockResolver.Wiremock(factory = StaticPortWiremockFactory.class) WireMockServer server) throws Exception {
+        setup(server);
+        addSearchStub(server,missingSurnameResourceFile);
+
+        MvcResult postResponse = mockMvc
+            .perform(
+                post("/supplementary-details")
+                    .content(missingSurnameResponseRequest)
+                    .contentType("application/json")
+                    .header("Authorization", equalTo("Bearer " + IntegrationConstants.USER_TOKEN))
+                    .header("ServiceAuthorization", equalTo("Bearer " + IntegrationConstants.SERVICE_TOKEN))
+            )
+            .andExpect(status().isOk())
+            .andReturn();
+
+        assertEquals(missingSurnameResponse, postResponse.getResponse().getContentAsString());
+
+    }
+
 }
