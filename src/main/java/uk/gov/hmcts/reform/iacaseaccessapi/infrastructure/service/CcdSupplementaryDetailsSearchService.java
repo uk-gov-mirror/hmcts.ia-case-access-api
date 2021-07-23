c72f883b508d9794e3f33cfd72d737af6e339d99
makowalski <marcin.kowalski.it.consulting@gmail.com>
2021-07-23 12:11:36 +0100

    Add query record limit in CCD ES request (#37)

--- src/main/java/uk/gov/hmcts/reform/iacaseaccessapi/infrastructure/service/CcdSupplementaryDetailsSearchService.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseaccessapi/infrastructure/service/CcdSupplementaryDetailsSearchService.java
@@ -65,6 +65,8 @@ public class CcdSupplementaryDetailsSearchService implements SupplementaryDetail
 
         SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
         TermsQueryBuilder termQueryBuilder = QueryBuilders.termsQuery("reference", ccdCaseNumberList);
+        searchSourceBuilder.size(100);
+        searchSourceBuilder.from(0);
         searchSourceBuilder.sort("created_date", SortOrder.DESC);
         searchSourceBuilder.query(termQueryBuilder);
 
