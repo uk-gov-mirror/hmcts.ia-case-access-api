44a94754110c7109232248ce443a162b1ca9c9ed
Brad Sorour <48841470+bradsorour@users.noreply.github.com>
2021-08-25 16:03:51 +0100

    RIA-4683 pagination to support larger PBA reconciliation (#39)

--- src/main/java/uk/gov/hmcts/reform/iacaseaccessapi/infrastructure/service/CcdSupplementaryDetailsSearchService.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseaccessapi/infrastructure/service/CcdSupplementaryDetailsSearchService.java
@@ -1,13 +1,20 @@
 package uk.gov.hmcts.reform.iacaseaccessapi.infrastructure.service;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.index.query.TermsQueryBuilder;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.elasticsearch.search.sort.SortOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.iacaseaccessapi.domain.entities.SupplementaryDetails;
@@ -24,18 +31,24 @@ import uk.gov.hmcts.reform.iacaseaccessapi.infrastructure.security.oauth2.Identi
 public class CcdSupplementaryDetailsSearchService implements SupplementaryDetailsService {
     private static final String CASE_TYPE_ID = "Asylum";
     private static final String APPELLANT_FAMILY_NAME = "appellantFamilyName";
-
     private final CoreCaseDataApi coreCaseDataApi;
     private final SystemTokenGenerator systemTokenGenerator;
     private final AuthTokenGenerator s2sAuthTokenGenerator;
+    private final int maxRecords;
+    private final ExecutorService executorService;
 
-
+    @Autowired
     public CcdSupplementaryDetailsSearchService(SystemTokenGenerator systemTokenGenerator,
                                                 CoreCaseDataApi coreCaseDataApi,
-                                                AuthTokenGenerator s2sAuthTokenGenerator) {
+                                                AuthTokenGenerator s2sAuthTokenGenerator,
+                                                @Qualifier("fixedThreadPool") ExecutorService executorService,
+                                                @Value("${requestPagination.maxRecords}") int maxRecords
+    ) {
         this.systemTokenGenerator = systemTokenGenerator;
         this.coreCaseDataApi = coreCaseDataApi;
         this.s2sAuthTokenGenerator = s2sAuthTokenGenerator;
+        this.executorService = executorService;
+        this.maxRecords = maxRecords;
     }
 
     @Override
@@ -63,14 +76,38 @@ public class CcdSupplementaryDetailsSearchService implements SupplementaryDetail
             throw new IdentityManagerResponseException(e.getMessage(), e);
         }
 
-        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
-        TermsQueryBuilder termQueryBuilder = QueryBuilders.termsQuery("reference", ccdCaseNumberList);
-        searchSourceBuilder.size(100);
-        searchSourceBuilder.from(0);
-        searchSourceBuilder.sort("created_date", SortOrder.DESC);
-        searchSourceBuilder.query(termQueryBuilder);
+        List<CompletableFuture<List<SupplementaryInfo>>> completableFutureList = new ArrayList<>();
+        final Collection<List<String>> chunkedCcdCaseList = getChunkedCcdCaseList(ccdCaseNumberList);
 
-        return search(userToken, s2sToken, searchSourceBuilder.toString());
+        for (List<String> splitCcdCaseList : chunkedCcdCaseList) {
+            CompletableFuture<List<SupplementaryInfo>> completableFuture = CompletableFuture.supplyAsync(
+                () -> {
+                    SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+                    TermsQueryBuilder termQueryBuilder = QueryBuilders.termsQuery("reference", splitCcdCaseList);
+                    searchSourceBuilder.size(maxRecords);
+                    searchSourceBuilder.from(0);
+                    searchSourceBuilder.sort("created_date", SortOrder.DESC);
+                    searchSourceBuilder.query(termQueryBuilder);
+
+                    return search(userToken, s2sToken, searchSourceBuilder.toString());
+                },
+                executorService
+            );
+            completableFutureList.add(completableFuture);
+        }
+
+        List<SupplementaryInfo> allResults = new ArrayList<>();
+
+        for (CompletableFuture<List<SupplementaryInfo>> completableFutureResult : completableFutureList) {
+            try {
+                allResults.addAll(completableFutureResult.get());
+            } catch (Exception e) {
+                Thread.currentThread().interrupt();
+                log.error(e.getMessage(), e);
+            }
+        }
+
+        return allResults;
     }
 
     private List<SupplementaryInfo> search(String userAuthorisation, String serviceAuthToken, String query) {
@@ -97,5 +134,14 @@ public class CcdSupplementaryDetailsSearchService implements SupplementaryDetail
             String.valueOf(caseDetails.getId()),
             new SupplementaryDetails(String.valueOf(caseDetails.getCaseData().get(APPELLANT_FAMILY_NAME)))
         );
+    }
+
+    private Collection<List<String>> getChunkedCcdCaseList(List<String> ccdCaseNumberList) {
+
+        final AtomicInteger counter = new AtomicInteger();
+
+        return ccdCaseNumberList.stream()
+            .collect(Collectors.groupingBy(it -> counter.getAndIncrement() / maxRecords))
+            .values();
     }
 }
